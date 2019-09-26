6a02f6aa6ab24d7ffae80cbdcca36a640f6b293c
Paddy Carver <paddy@carvers.co>
2019-09-26 01:58:58 -0700

    First provider_meta test written.
    Figure out how to test provider_meta, and write our first test, a basic
    check that the value will be delivered as part of the
    ApplyResourceChange request when set. Still a lot of test cases that
    need testing.

--- terraform/context_apply_test.go
+++ terraform/context_apply_test.go
@@ -28,6 +28,7 @@ import (
 	"github.com/hashicorp/terraform/states"
 	"github.com/hashicorp/terraform/tfdiags"
 	"github.com/zclconf/go-cty/cty"
+	"github.com/zclconf/go-cty/cty/gocty"
 )
 
 func TestContext2Apply_basic(t *testing.T) {
@@ -10405,5 +10406,56 @@ func TestContext2Apply_issue19908(t *testing.T) {
 	}
 	if got, want := obj.AttrsJSON, []byte(`"old"`); !bytes.Contains(got, want) {
 		t.Errorf("test.foo attributes JSON doesn't contain %s after apply\ngot: %s", want, got)
+	}
+}
+
+func TestContext2Apply_ProviderMeta_set(t *testing.T) {
+	m := testModule(t, "provider-meta-set")
+	p := testProvider("test")
+	p.ApplyFn = testApplyFn
+	p.DiffFn = testDiffFn
+	schema := p.GetSchemaReturn
+	schema.ProviderMeta = &configschema.Block{
+		Attributes: map[string]*configschema.Attribute{
+			"baz": {
+				Type:     cty.String,
+				Required: true,
+			},
+		},
+	}
+	p.GetSchemaReturn = schema
+	ctx := testContext2(t, &ContextOpts{
+		Config: m,
+		ProviderResolver: providers.ResolverFixed(
+			map[string]providers.Factory{
+				"test": testProviderFuncFixed(p),
+			},
+		),
+	})
+
+	if _, diags := ctx.Plan(); diags.HasErrors() {
+		t.Fatalf("plan errors: %s", diags.Err())
+	}
+
+	_, diags := ctx.Apply()
+	if diags.HasErrors() {
+		t.Fatalf("diags: %s", diags.Err())
+	}
+	if !p.ApplyResourceChangeCalled {
+		t.Fatalf("ApplyResourceChange not called")
+	}
+	if p.ApplyResourceChangeRequest.ProviderMeta.IsNull() {
+		t.Fatalf("null ProviderMeta in ApplyResourceChange")
+	}
+	type metaStruct struct {
+		Baz string `cty:"baz"`
+	}
+	var meta metaStruct
+	err := gocty.FromCtyValue(p.ApplyResourceChangeRequest.ProviderMeta, &meta)
+	if err != nil {
+		t.Fatalf("Error parsing cty value: %s", err)
+	}
+	if meta.Baz != "quux" {
+		t.Fatalf("Expected meta.Baz to be \"quux\", got %q", meta.Baz)
 	}
 }
